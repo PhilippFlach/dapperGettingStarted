58e053638320c31a8ec27d75ee15a0ce5cff9573
Philipp Flach <flach.philipp@luzi.ch>
2020-10-18 20:58:18 +0200

    Base classes in Data layer and Runner

---
+++ 03/demos/demos/before/DapperDemo/DataLayer/Address.cs
@@ -0,0 +1,17 @@
+﻿namespace DataLayer
+{
+    public class Address
+    {
+        public int ID { get; set; }
+        public int ContactID { get; set; }
+        public string AddressType { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public int StateID { get; set; }
+        public string PostalCode { get; set; }
+        internal bool IsNew => (this.ID == default(int));
+
+        public bool IsDeleted { get; set; }
+
+    }
+}
---
+++ 03/demos/demos/before/DapperDemo/DataLayer/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataLayer
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public string Title { get; set; }
+        public bool IsNew => this.ID == default(int);
+
+        public List<Address> Addresses { get; } = new List<Address>();
+    }
+}
