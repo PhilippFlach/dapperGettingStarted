f111795348e92f1809427afffcf302bebe103a43
Philipp Flach <flach.philipp@luzi.ch>
2020-10-19 22:00:32 +0200

    Initial query successfully tested

--- 03/demos/demos/before/DapperDemo/DataLayer/ContactRepository.cs
+++ 03/demos/demos/before/DapperDemo/DataLayer/ContactRepository.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 
 namespace DataLayer
 {
     public class ContactRepository : IContactRepository
     {
+        private IDbConnection db;
+        public ContactRepository(string connString)
+        {
+            if (connString is null)
+            {
+                throw new ArgumentNullException(nameof(connString));
+            }
+            this.db = new SqlConnection(connString);
+        }
+        public Contact Add(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contact> GetAll()
+        {
+            return this.db.Query<Contact>("SELECT * FROM Contacts").ToList();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact Update(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
